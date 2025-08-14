d359c274dfc3327539465332fff52e3e0f9c3c49
bananamans <33798289+bananamans@users.noreply.github.com>
2025-08-14 08:49:11 +0800

    update src file

---
+++ src/config.ts
@@ -0,0 +1,60 @@
+export const siteConfig = {
+  name: "Josh Ting Siong Lung",
+  title: "Software Engineer",
+  description: "Portfolio website of Josh Ting Siong Lung", //#1d4ed8
+  accentColor: "#3bb34fff",
+  social: {
+    email: "josh050507@gmail.com",
+    linkedin: "https://www.linkedin.com/in/josh-ting-173258227/",
+    github: "https://github.com/bananamans",
+  },
+  aboutMe:
+    "I am a recent Computer Science (Data Engineering) graduate from UNIMAS. For my final year project in university, I developed a hybrid AI recommendation model using deep learning, combining Content-Based Filtering and Neural Collaborative Filtering to improve recommendation accuracy. Beyond coding, I’m driven by curiosity and adaptability to grow both as a developer and a collaborator.",
+  skills: ["React", "PHP", "Flask", "ASP.NET", "Java", "MySQL", "PowerBI", "Machine Learning"],
+  projects: [
+    {
+      name: "E-commerce site with AI recommendation model",
+      description:
+        "A e-commerce prototype utilizing a deep learning recommendation model to enhance user experience and product discovery.",
+      link: "https://github.com/bananamans/E-commerce_prototype_with_flask",
+      skills: [],
+    },
+    {
+      name: "Event management site for event booking",
+      description:
+        "A platform for managing and booking events using PHP.",
+      link: "https://github.com/bananamans/Event_Management_Project",
+      skills: [],
+    },
+    {
+      name: "Movie recommendation site utilizing Natural Language Processing (NLP)",
+      description:
+        "A movie recommendation site built using NLP and hybrid recommendation model on Streamlit.",
+      link: "https://github.com/bananamans/movie_recommendation_system",
+      skills: [],
+    },
+  ],
+  experience: [
+    {
+      company: "Microlink Solutions Bhd.",
+      title: "Software Engineer Intern",
+      dateRange: "Feb 2024 - Aug 2024",
+      bullets: [
+        "Contributed to the development of the banking system, focusing on coding, debugging, and feature implementation.",
+        "Assisted in resolving front-end UI issues and some underlying bugs.",
+        "Collaborated with team members through Git-based version control to streamline development.",
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "Universiti Malaysia Sarawak (UNIMAS)",
+      degree: "Bachelor in Computer Science (Data Engineering)",
+      dateRange: "2021 - 2025",
+      achievements: [
+        "Graduated with 3.8 CGPA",
+        "Dean's List all semesters",
+      ],
+    },
+  ],
+};
